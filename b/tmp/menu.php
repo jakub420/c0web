3fa7c1bed5591b0e8b75a019f24c6bd638a3581d
jakub420 <73962143+jakub420@users.noreply.github.com>
2020-11-04 23:09:25 +0100

    u

--- b/tmp/menu.php
+++
@@ -1,8 +0,0 @@
-
- <a href="http://c0rrupt.cz/index.php">Home&nbsp;&nbsp;</a>
- <a href="http://c0rrupt.cz/soubory-files/files.php">Files&nbsp;</a>
- <a href="http://c0rrupt.cz/voip.php">VoIP TS3</a>
- <a href="http://c0rrupt.cz/profile/profile.php">Contacts</a>
- <a href="http://c0rrupt.cz/community.php">Community</a>
- <a href="http://c0rrupt.cz/forum-t.php">Forum&nbsp;</a>        
-
